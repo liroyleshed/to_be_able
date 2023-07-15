4092ef610ff6ad64d118b4ece02b5c9fbf6a242c
Liroy Leshed <liroyleshed@protonmail.com>
2023-07-15 16:03:23 -0700

    Add dsl

--- lib/to_be_able/ability.rb
+++ lib/to_be_able/ability.rb
@@ -13,4 +13,20 @@ module ToBeAble::Ability
   def cant?
     !can?
   end
+
+
+  def is_able_to
+    can
+  end
+
+  def isnt_able_to
+    !is_able_to
+  end
+
+  def is_able_to?
+  end
+
+  def isnt_able_to?
+    !is_able_to?
+  end
 end
