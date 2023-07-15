1b6abb928fece88918795ec9e54aa951eca6d7d8
Liroy Leshed <liroyleshed@protonmail.com>
2023-07-15 12:23:42 -0700

    Update permission.rb

--- lib/to_be_able/permission.rb
+++ lib/to_be_able/permission.rb
@@ -1,9 +1,7 @@
-module ToBeAble
-  class Permission
-    attr_reader :law
+class ToBeAble::Permission
+  attr_reader :law
 
-    def initialize(law)
-      @law = law
-    end
+  def initialize(law)
+    @law = law
   end
 end
