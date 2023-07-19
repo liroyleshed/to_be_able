a53690872de8507a08a1746fe86b5c3e44796c2d
Liroy Leshed <liroyleshed@protonmail.com>
2023-07-19 00:37:03 -0700

    Update permission.rb

--- lib/to_be_able/permission.rb
+++ lib/to_be_able/permission.rb
@@ -5,3 +5,10 @@ class ToBeAble::Permission
     @law = law
   end
 end
+
+class ToBeAble::Permission::Permitted < ToBeAble::Permission
+end
+
+
+class ToBeAble::Permission::Unpermitted < ToBeAble::Permission
+end
