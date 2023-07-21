3656517a970bc0edfe51d55812df7d470316a4e3
Liroy Leshed <liroyleshed@protonmail.com>
2023-07-21 13:58:50 -0700

    Update database_adapter.rb

--- lib/to_be_able/database_adapter.rb
+++ lib/to_be_able/database_adapter.rb
@@ -26,23 +26,23 @@ module ToBeAble
 
       # Used to determine if this model adapter will override the matching behavior for a hash of conditions.
       # If this returns true then matches_conditions_hash? will be called.
-      def override_conditions_hash_matching?(subject, conditions)
+      def override_conditions_hash_matching?(resource, conditions)
         false
       end
 
       # Override if override_conditions_hash_matching? returns true
-      def matches_conditions_hash?(subject, conditions)
+      def matches_conditions_hash?(resource, conditions)
         raise NotImplemented, "This model adapter does not support matching on a conditions hash."
       end
 
       # Used to determine if this model adapter will override the matching behavior for a specific condition.
       # If this returns true then matches_condition? will be called.
-      def override_condition_matching?(subject, name, value)
+      def override_condition_matching?(resource, name, value)
         false
       end
 
       # Override if override_condition_matching? returns true
-      def matches_condition?(subject, name, value)
+      def matches_condition?(resource, name, value)
         raise NotImplemented, "This model adapter does not support matching on a specific condition."
       end
     end
