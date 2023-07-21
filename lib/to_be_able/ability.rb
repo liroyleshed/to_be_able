ea62c9376e8d03987fac0b53f054c5c2ca800b08
Liroy Leshed <liroyleshed@protonmail.com>
2023-07-20 23:52:52 -0700

    local vars

--- lib/to_be_able/ability.rb
+++ lib/to_be_able/ability.rb
@@ -1,12 +1,12 @@
 module ToBeAble::Ability
   def can(perform_action, on_resource)
-    @law = ToBeAble::Law.new(permitted: true, action: perform_action, resource: on_resource)
-    registry.register(@law)
+    law = ToBeAble::Law.new(permitted: true, action: perform_action, resource: on_resource)
+    registry.register(law)
   end
 
   def cant(perform_action, on_resource)
-    @law = ToBeAble::Law.new(permitted: false, action: perform_action, resource: on_resource)
-    registry.register(@law)
+    law = ToBeAble::Law.new(permitted: false, action: perform_action, resource: on_resource)
+    registry.register(law)
   end
 
 
