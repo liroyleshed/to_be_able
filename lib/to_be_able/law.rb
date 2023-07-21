4ae92dbf868952ee39c345ae62ebaa7765cc75c8
Liroy Leshed <liroyleshed@protonmail.com>
2023-07-20 23:24:50 -0700

    Update law.rb

--- lib/to_be_able/law.rb
+++ lib/to_be_able/law.rb
@@ -1,7 +1,7 @@
 class ToBeAble::Law
   def  initialize(permitted: false, action: perform_action, resource: on_resource)
-    @permitted = permitted
-    @action    = perform_action
-    @resource  = on_resource
+    @permitted      = permitted
+    @perform_action = perform_action
+    @on_resource    = on_resource
   end
 end
